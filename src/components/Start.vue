2dd8c7a9e6f9efdb6dec79d36012a980e7c55d0d
Jesus <navarro.jesusalfredo@gmail.com>
2023-01-24 17:39:58 -0600

    transform enable and multiple shapes

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,26 +1,29 @@
 <template>
-<v-circle :config="configCircle"></v-circle>
+<v-circle :config="configCircle" @transformend="sendEvent"></v-circle>
 </template>
 
 <script lang="js">
 export default {
   name: 'Start',
+  emits: ["transformend"],
+  props: {
+    configCircle: Object,
+  },
+  mounted() {
+    
+  },
   data() {
     return {
       configKonva: {
         width: 200,
         height: 200
       },
-      configCircle: {
-        x: 100,
-        y: 100,
-        radius: 100,
-        fill: "white",
-        stroke: "black",
-        strokeWidth: 4,
-        draggable: true
-      }
     };
+  },
+  methods: {
+    sendEvent(e) {
+      this.$emit("transformend", e);
+    }
   }
 };
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,8 @@
 <v-stage 
   ref="stage" 
   :config="stageSize"
-  class="stage"
+  @mousedown="handleStageMouseDown"
+  @touchstart="handleStageMouseDown"
   >
       <v-layer 
       ref="layer"
@@ -10,10 +11,16 @@
       >
 
       <!-- Diagram -->
-      <!-- <Start /> -->
+      <Start
+        v-for="item in shapes.starts"
+        :key="item.id"
+        :configCircle="item"
+        @transformend="handleTransformEnd"
+      />
       <!-- <End /> -->
       <!-- <Decision /> -->
-        <Process />
+      <!--  <Process /> -->
+      <v-transformer ref="transformer" />
       </v-layer>
     </v-stage>
 </template>
@@ -21,6 +28,7 @@
 <script>
 const width = window.innerWidth;
 const height = window.innerHeight;
+import Konva from 'konva';
 import Start from '../components/Start.vue';
 import End from '../components/End.vue';
 import Decision from '../components/Decision.vue';
@@ -33,7 +41,31 @@ export default {
         width: width,
         height: height
       },
-      isDragging: false
+      isDragging: false,
+      shapes: {
+        starts: [
+          {
+            x: 100,
+            y: 100,
+            radius: 100,
+            fill: "white",
+            stroke: "black",
+            strokeWidth: 4,
+            draggable: true,
+            name: "inicio",
+          },
+          {
+            x: 100,
+            y: 100,
+            radius: 100,
+            fill: "white",
+            stroke: "black",
+            strokeWidth: 4,
+            draggable: true,
+            name: "inicio2",
+          },
+        ]
+      },
     };
   },
   components: {Start, End, Decision, Process},
@@ -44,10 +76,79 @@ export default {
     handleDragEnd() {
       this.isDragging = false;
     },
-    exampleFunction() {
-      console.log("Hola");
-    }
+    handleTransformEnd(e) {
+      // shape is transformed, let us save new attrs back to the node
+      // find element in our state
+      const shape = this.getShape(this.selectedShapeName);
+      // update the state
+      shape.x = e.target.x();
+      shape.y = e.target.y();
+      shape.rotation = e.target.rotation();
+      shape.scaleX = e.target.scaleX();
+      shape.scaleY = e.target.scaleY();
 
+      // change fill
+      shape.fill = Konva.Util.getRandomColor();
+    },
+    handleStageMouseDown(e) {
+      // clicked on stage - clear selection
+      if (e.target === e.target.getStage()) {
+        this.selectedShapeName = '';
+        this.updateTransformer();
+        return;
+      }
+
+      // clicked on transformer - do nothing
+      const clickedOnTransformer =
+        e.target.getParent().className === 'Transformer';
+      if (clickedOnTransformer) {
+        return;
+      }
+
+      // find clicked shape by its name
+      const name = e.target.name();
+      const shape = this.getShape(name);
+      if (shape) {
+        this.selectedShapeName = name;
+      } else {
+        this.selectedShapeName = '';
+      }
+      this.updateTransformer();
+    },
+    updateTransformer() {
+      // here we need to manually attach or detach Transformer node
+      const transformerNode = this.$refs.transformer.getNode();
+      const stage = transformerNode.getStage();
+      const { selectedShapeName } = this;
+
+      const selectedNode = stage.findOne('.' + selectedShapeName);
+      // do nothing if selected node is already attached
+      if (selectedNode === transformerNode.node()) {
+        return;
+      }
+
+      if (selectedNode) {
+        // attach to another node
+        transformerNode.nodes([selectedNode]);
+      } else {
+        // remove transformer
+        transformerNode.nodes([]);
+      }
+    },
+    getShape(name) {
+      let shape = undefined;
+      for (const s in this.shapes) {
+        shape = this.shapes[s].find( (r) => {
+          if(r.name === name){
+            return r;
+          }
+        });
+        if(shape != undefined) {
+          break;
+        }
+      }
+      return shape;
+    },
   }
 };
 </script>
